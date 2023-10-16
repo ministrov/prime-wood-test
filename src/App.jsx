e515d37379477c8f7e0bba38c2eafb82b1a6a690
ministrov <61511030+ministrov@users.noreply.github.com>
2023-10-16 13:55:01 +0300

    feat: create header and  table components and start to style it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css'
+import Header from './components/Header/Header';
+import Table from './components/Table/Table';
+import './App.css';
 
 function App() {
 
   return (
-    <div>Hello</div>
+    <main>
+      <div className="container">
+        <Header />
+        <Table />
+      </div>
+    </main>
   )
 }
 
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,10 @@
+import './Table.css';
+
+function Table() {
+
+  return (
+    <div>Table</div>
+  )
+}
+
+export default Table;
